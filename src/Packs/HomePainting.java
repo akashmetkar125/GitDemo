bfbb2b68d748652c05f2bc741795a2e7d9eaf851
akashmetkar125 <akashrmetkar121@gmail.com>
2022-08-15 16:35:19 +0530

    First Commit

---
+++ src/Packs/HomePainting.java
@@ -0,0 +1,25 @@
+package Packs;
+
+public class HomePainting {
+
+	
+public static void main(String[] args) {
+	
+	HomePainting hp= new HomePainting();
+	hp.Color();
+	hp.Brand();
+	
+}
+
+public void Color() {
+	
+	System.out.println("This is Red");
+}
+
+public void Brand() {
+	
+	System.out.println("Asian Paints");
+}
+
+
+}
