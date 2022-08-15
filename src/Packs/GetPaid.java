9c5703830caf9f83fb23da0237a837ca6775e13a
akashmetkar125 <akashrmetkar121@gmail.com>
2022-08-15 17:19:03 +0530

    Final Method added GProject

---
+++ src/Packs/GetPaid.java
@@ -0,0 +1,15 @@
+package Packs;
+
+public class GetPaid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public void Money() {
+		
+		System.out.println("Get Paid Out");
+	}
+}
